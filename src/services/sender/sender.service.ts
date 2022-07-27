3e6f9a8239a8a2d33aa6584deb9dc64fd2e16210
= <mypochtav@gmail.com>
2022-07-27 12:44:26 +0300

    remove EMAIL from import

--- src/services/sender/sender.service.ts
+++ src/services/sender/sender.service.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { EMAIL, USER, PASS } from "../../configs/config";
+import { USER, PASS } from "../../configs/config";
 import {
   SendEmailPropsInterface,
   SenderServiceInterface,
