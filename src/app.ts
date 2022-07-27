3d4d73268e012de6740af4597460e9b84cc9cef2
= <mypochtav@gmail.com>
2022-07-27 15:58:34 +0300

    add docker and logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,40 @@
-import express, { Express, Request, Response } from "express";
+import express, {Express, Request, Response} from "express";
 import cors from "cors";
 import cryptoAPIController from "./controllers/cryptoAPI";
 import multer from "multer";
 import emailsController from "./controllers/emails";
 import senderController from "./controllers/sender";
+import logger from "./middleware/logger/logger";
 
 const bootstrap = (app: Express) => {
-  app.use(cors());
-  app.use(express.json());
+    app.use(cors());
+    app.use(express.json());
+    app.use(logger)
 
-  app.get(
-    "/rate",
-    async (req: Request, res: Response) =>
-      await cryptoAPIController.rate(req, res)
-  );
+    app.get(
+        "/rate",
+        async (req: Request, res: Response) =>
+            await cryptoAPIController.rate(req, res)
+    );
 
-  app.post(
-    "/subscribe",
-    multer().none(),
-    async (req: Request, res: Response) =>
-      await emailsController.subscribe(req, res)
-  );
+    app.post(
+        "/subscribe",
+        multer().none(),
+        async (req: Request, res: Response) =>
+            await emailsController.subscribe(req, res)
+    );
 
-  app.get(
-    "/rate",
-    async (req: Request, res: Response) =>
-      await cryptoAPIController.rate(req, res)
-  );
+    app.get(
+        "/rate",
+        async (req: Request, res: Response) =>
+            await cryptoAPIController.rate(req, res)
+    );
 
-  app.post(
-    "/sendEmails",
-    async (req: Request, res: Response) =>
-      await senderController.sendEmails(req, res)
-  );
+    app.post(
+        "/sendEmails",
+        async (req: Request, res: Response) =>
+            await senderController.sendEmails(req, res)
+    );
 };
 
 export default bootstrap;
---
+++ src/middleware/logger/logger.ts
@@ -0,0 +1,13 @@
+import {Request, Response, NextFunction} from "express";
+
+const logger = function (req: Request, res: Response, next: NextFunction) {
+    const protocol = req.protocol;
+    const host = req.hostname;
+    const url = req.originalUrl;
+    const port = process.env.PORT;
+    const fullUrl = `${protocol}://${host}:${port}${url}`
+    console.log(`[logger]:  DATE: ${new Date().toLocaleTimeString()} URL: ${fullUrl}`)
+    next();
+};
+
+export default logger;
